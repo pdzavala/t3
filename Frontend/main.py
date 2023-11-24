6b2ef18e0b2771424a3c08182874829a9342193c
pdzavala <48297426+pdzavala@users.noreply.github.com>
2023-11-23 21:19:25 -0300

    Verify

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,6 +1,20 @@
-from typing import Optional
-
+import os
+from google.cloud import storage
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_APPLICATION_CREDENTIAL = os.getenv('GOOGLE_APPLICATION_CREDENTIAL')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+class Bucket:
+    def __init__(self, storage_client):
+        self.client = storage_client
+
+    def list_blobs(self, bucket_name):
+        blobs = self.client.list_blobs(bucket_name)
+        return blobs
 
 app = FastAPI()
 
@@ -12,3 +26,6 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+for blob in Bucket.list_blobs(BUCKET_NAME):
+    print(blob.name)
